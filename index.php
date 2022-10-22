1b19b473308860b42e45bf62529641317ed8b00a
Michal <msredzinski2003@gmail.com>
2022-10-22 14:26:37 +0200

    added debug functions

--- index.php
+++ index.php
@@ -1,2 +1,16 @@
 <?php
-echo "test";
+
+
+declare(strict_types=1);
+
+namespace App;
+
+include_once('.src/utils/debug.php');
+// include
+// include_once
+// require
+// require_once
+
+
+dump('test');
+dump('==== dump ====');
