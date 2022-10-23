a732eb2af8a6fcf62c623e8a3e35f176a5545546
Michal <msredzinski2003@gmail.com>
2022-10-23 13:58:13 +0200

    error display and render change

--- index.php
+++ index.php
@@ -5,6 +5,10 @@ declare(strict_types=1);
 
 namespace App;
 
+// to powinno być na wersji na świat. Dla nas błędy są ważne.
+// error_reporting(0);
+// ini_set('display_errors', '0');
+
 require_once('./src/view.php');
 include_once('.src/utils/debug.php');
 
@@ -15,10 +19,12 @@ $action = $_GET['action'] ?? DEFAULT_ACTION;
 $viewParams = [];
 
 if ($action === 'create') {
+    $page = 'create';
     $viewParams['resultCreate'] = 'udało się dodać notatkę!';
 } else {
+    $page = 'list';
     $viewParams['resultList'] = 'Wyświetlamy listę notatek!';
 }
 
 $view = new View();
-$view->render($action, $viewParams);
+$view->render($page, $viewParams);
