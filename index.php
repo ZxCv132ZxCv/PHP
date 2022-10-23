5b26da0b5c02a0534b1af9f45e7e45ad6394c182
Michal <msredzinski2003@gmail.com>
2022-10-23 17:19:25 +0200

    adding controller

--- index.php
+++ index.php
@@ -9,30 +9,8 @@ namespace App;
 // error_reporting(0);
 // ini_set('display_errors', '0');
 
-require_once('./src/view.php');
+require_once('./src/Controller.php');
 include_once('./src/utils/debug.php');
 
-const DEFAULT_ACTION = 'list';
-
-$action = $_GET['action'] ?? DEFAULT_ACTION;
-
-$viewParams = [];
-
-if ($action === 'create') {
-    $page = 'create';
-    $created = false;
-    if (!empty($_POST)) {
-        $viewParams = [
-            'title' => $_POST['title'],
-            'description' => $_POST['description'],
-        ];
-        $created = true;
-    }
-    $viewParams['created'] = $created;
-} else {
-    $page = 'list';
-    $viewParams['resultList'] = 'Wyświetlamy listę notatek!';
-}
-
-$view = new View();
-$view->render($page, $viewParams);
+$controller = new Controller($_GET, $_POST);
+$controller->run();
