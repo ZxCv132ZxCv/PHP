54072e528ad8c0360556a84fefc0dfb8524fe2fa
Michal <msredzinski2003@gmail.com>
2022-11-06 14:12:13 +0100

    adding App, Configuration and Storage Exceptions

--- index.php
+++ index.php
@@ -9,11 +9,25 @@ namespace App;
 // error_reporting(0);
 // ini_set('display_errors', '0');
 
+require_once('./Exception/AppException.php');
+require_once('./Exception/StorageException.php');
+require_once('./Exception/ConfigurationException.php');
 require_once('./src/Controller.php');
 include_once('./src/utils/debug.php');
 require_once('./config/config.php');
 
-Controller::initConfiguration($configuration);
+use App\Exception\AppException;
+use App\Exception\StorageException;
+use App\Exception\ConfigurationException;
+use Throwable;
 
-$controller = new Controller($_GET, $_POST);
-$controller->run();
+try {
+    Controller::initConfiguration($configuration);
+    $controller = new controller($_GET, $_POST);
+    $controller->run();
+} catch (AppException $e) {
+    echo "<h1>Wystąpił błąd w aplikacji</h1>";
+    echo '<h3>' . $e->getMessage() . '</h3';
+} catch (Throwable $e) {
+    echo "<h1>Wystąpił błąd w aplikacji</h1>";
+}
