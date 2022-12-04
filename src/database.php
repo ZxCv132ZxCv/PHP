002eebc98193050f25d83ca31089f9f74939f760
Michal <msredzinski2003@gmail.com>
2022-12-04 13:35:10 +0100

    add editNote function

--- src/database.php
+++ src/database.php
@@ -65,6 +65,19 @@ class Database
             throw new StorageException('Nie udało się pobrać danych o notatkach', 400, $e);
         }
     }
+    public function editNote(int $id, array $data): void
+    {
+        try {
+            $title = $this->conn->quote($data['title']);
+            $description = $this->conn->quote($data['description']);
+
+            $query = "UPDATE notes SET title = $title, description = $description WHERE id = $id";
+
+            $this->conn->exec($query);
+        } catch (Throwable $e) {
+            throw new StorageException('Nie udało się edytować notatki', 400, $e);
+        }
+    }
     private function createConnection(array $config): void
     {
         $dsn = "mysql:dbname={$config['database']};host={$config['host']}";
