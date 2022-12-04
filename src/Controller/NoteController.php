66bbf5ba28bc7d8608e9645085b0c3b68903932b
Michal <msredzinski2003@gmail.com>
2022-12-04 14:34:46 +0100

    add DeleteNote function, add delete file

--- src/Controller/NoteController.php
+++ src/Controller/NoteController.php
@@ -65,6 +65,25 @@ class NoteController extends AbstractController
 
             $this->redirect('/', ['before' => 'edited']);
         }
+        $this->view->render(
+            'edit',
+            ['note' => $this->getNote()],
+        );
+    }
+    public function deleteAction(): void
+    {
+        if ($this->request->isPost()) {
+            $id = (int) $this->request->postParam('id');
+            $this->database->deleteNote($id);
+            $this->redirect('/', ['before' => 'deleted']);
+        }
+        $this->view->render(
+            'delete',
+            ['note' => $this->getNote()],
+        );
+    }
+    public function getNote(): array
+    {
         $noteId = (int) $this->request->getParam('id');
         if (!$noteId) {
             $this->redirect('/', ['error' => 'missingNoteId']);
@@ -73,11 +92,7 @@ class NoteController extends AbstractController
             $note = $this->database->getNote($noteId);
         } catch (NotFoundException $e) {
             $this->redirect('/', ['error' => 'noteNotFound']);
-            exit;
         }
-        $this->view->render(
-            'edit',
-            ['note' => $note],
-        );
+        return $note;
     }
 }
