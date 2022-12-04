5e30d2f7050175c88e52b070cd194016bc0c9936
Michal <msredzinski2003@gmail.com>
2022-12-04 11:59:28 +0100

    add edit function

--- src/Controller/NoteController.php
+++ src/Controller/NoteController.php
@@ -17,8 +17,7 @@ class NoteController extends AbstractController
                 'description' => $this->request->postParam('description')
             ];
             $this->database->CreateNote($noteData);
-            header('Location: /?before=created');
-            exit;
+            $this->redirect('/', ['before' => 'created']);
         }
         $this->view->render('create');
     }
@@ -26,14 +25,12 @@ class NoteController extends AbstractController
     {
         $noteId = (int) $this->request->getParam('id');
         if (!$noteId) {
-            header('Location: /?error=missingNoteId');
-            exit;
+            $this->redirect('/', ['error' => 'missingNoteId']);
         }
         try {
             $note = $this->database->getNote($noteId);
         } catch (NotFoundException $e) {
-            header('Location: /?error=noteNotFound');
-            exit;
+            $this->redirect('/', ['error' => 'noteNotFound']);
         }
         $viewParams = [
             'title' => 'MojaNotatka',
@@ -54,5 +51,15 @@ class NoteController extends AbstractController
             'before' => $this->request->getParam('before'),
             'error' => $this->request->getParam('error'),
         ]);
+    }
+    public function editAction()
+    {
+        $noteId = (int) $this->request->getParam('id');
+        if (!$noteId) {
+            $this->redirect('/', ['error' => 'missingNoteId']);
+        }
+        $this->view->render(
+            'edit'
+        );
     }
 }
